7bab1956d7eb72e84db45f2cbe855123dafe062c
Syahrul Amri <syahrul11aemry@gmail.com>
2025-05-08 14:20:04 +0700

    minor fix add logo metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
-"use client"
 import React from 'react';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import '@/styles/globals.css';
+
+export const metadata = {
+  title: "Syahrul Amri",
+  description: "Personal website of Syahrul Amri",
+  icons: {
+    icon: "/syahrullogo.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
